20e1d5129128832f1086d5b27bcba48cc487e2c8
Bastien Winant <bastien.winant@gmail.com>
2024-09-14 01:53:55 +0200

    add inventory and sale

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://sporcks.com/en-eu/',
+            inventory: 0,
+            onSale: false
         }
     }
 })
